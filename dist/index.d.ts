9580579c5490b43c063b94f94a50e513005ba6c8
royossai <royossai@yahoo.com>
2023-03-25 21:54:04 +0100

    added some comments

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,3 @@
 import { TmDBApi } from "./tmDB";
 import { hasData, hasError } from "./response";
-import { ImageApi } from "./imageApi";
-export { TmDBApi, hasData, hasError, ImageApi };
+export { TmDBApi, hasData, hasError };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { TmDBApi } from "./tmDB";
 import { hasData, hasError } from "./response";
-import { ImageApi } from "./imageApi";
 
-export { TmDBApi, hasData, hasError, ImageApi };
+export { TmDBApi, hasData, hasError };
